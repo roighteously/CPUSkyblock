a8e27059fa49cc66d43a152e9fc68b07f03e0821
Roizor <aidenjs9@icloud.com>
2023-07-26 18:13:18 -0500

    move to 1 file lol

--- src/main/java/crackpixel/org/skyblock/CPUSkyblock.java
+++ src/main/java/crackpixel/org/skyblock/CPUSkyblock.java
@@ -1,18 +1,41 @@
 package crackpixel.org.skyblock;
 
+import java.math.BigDecimal;
+
 import org.bukkit.Bukkit;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CPUSkyblock extends JavaPlugin {
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
+import com.bgsoftware.superiorskyblock.api.island.Island;
+import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
+
+public final class CPUSkyblock extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getPluginManager().registerEvents(new PortalEventListener(), this);
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPortal(PlayerPortalEvent portalEvent) {
+        Player p = portalEvent.getPlayer();
+        if (SuperiorSkyblockAPI.getPlayer(p).hasIsland()) {
+            Island i = SuperiorSkyblockAPI.getIsland(p.getName());
+            SuperiorSkyblockAPI.getPlayer(p).teleport(i);
+        } else {
+            SuperiorPlayer sp = SuperiorSkyblockAPI.getPlayer(p);
+            SuperiorSkyblockAPI.createIsland(sp, "normal", new BigDecimal("0"), Biome.PLAINS, p.getName());
+        }
     }
 }
